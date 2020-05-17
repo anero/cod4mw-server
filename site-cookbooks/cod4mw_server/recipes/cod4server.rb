4cec60a7debf7b79275a98f66c8d12610253ceb9
anero <anero79@gmail.com>
2020-05-17 20:17:15 -0300

    Agrego cfg para armas (WIP)

--- site-cookbooks/cod4mw_server/recipes/cod4server.rb
+++ site-cookbooks/cod4mw_server/recipes/cod4server.rb
@@ -8,6 +8,14 @@ cookbook_file '/home/cod4server/serverfiles/main/cod4server.cfg' do
   action :create
 end
 
+cookbook_file '/home/cod4server/serverfiles/main/weapons.cfg' do
+  owner 'cod4server'
+  group 'cod4server'
+  source 'weapons.cfg'
+  mode '0504'
+  action :create
+end
+
 # Start cod4server on init
 cookbook_file '/lib/systemd/system/cod4server.service' do
   source 'cod4server.service'
